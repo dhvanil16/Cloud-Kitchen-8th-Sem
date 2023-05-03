5eb393c4d12f8edec98b6e7e81a7df74f8f30da9
dhvanil16 <dhvanilp16@gmail.com>
2023-05-03 22:54:31 +0530

    Cloud Kitchen Done

---
+++ components/OrderDetail.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import styles from "../styles/OrderDetail.module.css";
+
+const OrderDetail = ({ total, createOrder }) => {
+  const [customer, setCustomer] = useState("");
+  const [address, setAddress] = useState("");
+
+  const handleClick = () => {
+    createOrder({ customer, address, total, method: 0 });
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>Give Your Details</h1>
+        <div className={styles.item}>
+          <label className={styles.label}>Name</label>
+          <input
+            placeholder="Enter your name"
+            type="text"
+            className={styles.input}
+            onChange={(e) => setCustomer(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Phone Number</label>
+          <input
+            type="text"
+            placeholder="Enter your phonenumber"
+            className={styles.input}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Address</label>
+          <textarea
+            rows={5}
+            placeholder="Enter your address"
+            type="text"
+            className={styles.textarea}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </div>
+        <button className={styles.button} onClick={handleClick}>
+          Order
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default OrderDetail;
---
+++ components/PizzaList.jsx
@@ -0,0 +1,20 @@
+import styles from "../styles/PizzaList.module.css";
+import PizzaCard from "./PizzaCard";
+
+const PizzaList = ({ pizzaList }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+      <p className={styles.desc}>
+      Welcome to "Patel's Pizza" - a haven for pizza lovers! Located in the heart of a bustling city, our cozy and vibrant restaurant is the ultimate destination for an unforgettable pizza experience.
+      </p>
+      <div className={styles.wrapper}>
+        {pizzaList.map((pizza) => (
+          <PizzaCard key={pizza._id} pizza={pizza} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PizzaList;
